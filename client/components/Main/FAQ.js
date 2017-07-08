bafd9b703639f67ac5afdd2fd75a313c5ff308cd
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-08 13:30:20 -0700

    added FAQ component

---
+++ client/components/Main/FAQ.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import {} from
+
+class FAQ extends Component {
+  render () {
+    return (
+      <div>
+        <h1>SERVICE AREA</h1>
+        <h1>UGLY PRODUCE</h1>
+        <h1>DELIVERY</h1>
+        <h1>USING OUR ORDERING SYSTEM</h1>
+        <h1>CONTACT</h1>
+      </div>
+    )
+  }
+}
+
+export default FAQ;
