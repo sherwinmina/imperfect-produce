af2f561adec35c2cc5fbe5f36d36688e23a9d1f8
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-07 20:52:55 -0700

    fixing dependecies

--- client/components/Account/AccountNav.js
+++ client/components/Account/AccountNav.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
 
+// import Foundation from 'react-foundation';
+
 class AccountNav extends Component {
   render () {
     return (
       <div>
-       <Link className="">My Account</Link>
-       <Link>Account Details</Link>
-       <Link>Billing Details</Link>
-       <Link>Delivery Schedule</Link>
-       <Link>Manage Subscription</Link>
-       <Link>Refer Friends</Link>
+        <Row>
+          <Link className="">My Account</Link>
+          <Link>Account Details</Link>
+          <Link>Billing Details</Link>
+          <Link>Delivery Schedule</Link>
+          <Link>Manage Subscription</Link>
+          <Link>Refer Friends</Link>
+        </Row>
+       
 
       <div className="display">
         
