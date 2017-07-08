fd55fdf8f11e6af67b328431d5873fa992c7355e
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-07 19:10:00 -0700

    Got the set up working

--- client/index.js
+++ client/index.js
@@ -4,23 +4,15 @@ import { Router, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './rootReducer';
-
 
 import routes from './routes';
 
 const store = createStore(
-  rootReducer,
   compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : f => f
   )
 );
-
-if (localStorage.jwtToken) {
-  setAuthorizationToken(localStorage.jwtToken);
-  store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)));
-}
 
 render(
   <Provider store={store}>
