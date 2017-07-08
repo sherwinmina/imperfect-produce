460b3c418a5837043ce261892fa3d82e424f9a58
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-07 18:54:14 -0700

    Initial set up with react

---
+++ client/components/Greetings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Greetings extends React.Component {
+  render() {
+    return (
+      <div className="jumbotron">
+        <h1>Greetings Component</h1>
+      </div>
+    );
+  }
+}
+
+export default Greetings;
--- client/routes.js
+++ client/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
+import Greetings from './components/Greetings';
 
 export default (
   <Route path="/" component={App}>
