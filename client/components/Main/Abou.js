e59325c79e230a08635616cead17f100abfc36ac
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-08 13:25:36 -0700

    added stateless component for contacts and about page

---
+++ client/components/Main/Abou.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const About = () => (
+ <div>  
+  <h2>WHAT WE’RE ALL ABOUT</h2>
+
+  <p>
+    In America, more than 20% of the fruits and vegetables we grow never make it off of farms because they aren’t Perfect enough for grocery stores’ standards.This results in billions of pounds of produce going to waste every year. We’re talking about produce that’s just as nutritious, and delicious as perfect produce that gets rejected just because it looks a little different. With so many delicious fruits and vegetables going to waste every year, we knew we had to do something to make an impact.
+    Imperfect fights food waste by finding a home for ‘ugly’ produce. We source it directly from farms and deliver it to customers’ doors for 30-50% less than grocery store prices. We offer a subscription produce box that’s affordable, convenient, customizable, and delicious. It’s bigger than a box, though. By eating Imperfect, you’re helping build a more sustainable and effective food system in the most delicious way imaginable. You’re helping fight food waste. You’re ensuring farmers are rewarded for their full harvest and can feed more people with less wasted land, fossil fuels, and water. You’re improving access to healthy food. You’re creating fulfilling careers for employees. With every bite into a misshapen apple or crooked carrot, you’re helping shape our world for the better.
+    In a world obsessed with perfection in everything, we’re here to show the world the beauty in imperfection. We’re here to stand up for the underdog: the curvy cucumber, the lumpy potato, the oversized grapefruit. We believe that they matter too. We’re here to make a statement that our differences are what make us beautiful. Just like people, produce comes in a wide range of shapes, sizes, and colors, and we think this diversity is awesome and something to celebrate, not waste. We invite you to join our community and help us build a less wasteful world that’s as beautiful as it is delicious!
+  </p>
+
+  <h2>OUR HISTORY</h2>
+  <p>
+    As a college student, our CEO Ben Simon noticed that a lot of food waste going to waste in the cafeteria at the University of Maryland. To address this problem, he founded the Food Recovery Network, a nonprofit dedicated to preventing waste on college campuses across the country. Since its founding the FRN has expanded to over 180 colleges and universities. Ben Simon met co-founder Ben Chesler during his work at with the FRN, and Ben Chesler helped him scale his mission of fighting food waste nationwide. During a trip to California in 2015, Ben and Ben met their third co-founder, Ron Clark. Ron had spent decades working in the produce industry, most recently working for the California Association of Food Banks. At the CAFB, Ron developed the “Farm to Family” program that now sources 125 million pounds a year of "ugly produce" to the food banks within the state of California. Through this work Ron had built out a supply chain of 70 to 80 California growers and had been sourcing a large amount of produce. Yet he realized that the quantities that he was getting to food banks were still a drop in the bucket compared to the billions of pounds that never left the farm went to waste. Ben, Ben, and Ron realized that together they could make a big impact on the amount of produce that goes to waste every year because of appearances by sourcing it directly from farms and delivering it at a discount to customers homes. They joined forces and founded Imperfect on August 8th, 2015, with the vision of finding a home for the 6 billion pounds of ugly produce that goes to waste each year in California alone.
+  </p>
+ 
+ </div>
+);
+
+export default About;
---
+++ client/components/Main/Contact.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Contact = () => (
+ <div>  
+  <h2>WE’RE EXCITED TO HEAR FROM YOU!</h2>
+
+  <h3>WE’RE ALWAYS OPEN TO SUGGESTIONS ON WAYS WE CAN MAKE OUR CUSTOMERS EVEN HAPPIER.</h3>
+ 
+  <p>
+  We also reply to press/media inquiries, job applications, and selfies with ugly produce. For the timeliest responses, please email the account most closely associated with your request.
+  </p>
+
+  <h5>Press/Media: press@imperfectproduce.com</h5>
+  <h5>Careers: hiring@imperfectproduce.com</h5>
+  <h5>Support/Questions/Help: support@imperfectproduce.com</h5>
+ 
+ </div>
+);
+
+export default Contact;
