9535c4fcfb2bc8471f0ee2372b172770b798786b
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-08 00:18:31 -0700

    link navigation with semantic ui

--- client/components/Account/AccountNav.js
+++ client/components/Account/AccountNav.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router';
 
-// import Foundation from 'react-foundation';
+import { Grid, Image } from 'semantic-ui-react'
 
 class AccountNav extends Component {
   render () {
     return (
       <div>
-        <Row>
-          <Link className="">My Account</Link>
+      
+        <div className="display">
+        
+         
+        </div>
+        <Grid celled>
+    <Grid.Row>
+      <Grid.Column width={3}>
+         <Link className="">My Account</Link>
           <Link>Account Details</Link>
           <Link>Billing Details</Link>
           <Link>Delivery Schedule</Link>
           <Link>Manage Subscription</Link>
           <Link>Refer Friends</Link>
-        </Row>
+      </Grid.Column>
+      <Grid.Column width={13}>
+        <Image src='/assets/images/wireframe/centered-paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+    <Grid.Row>
+      <Grid.Column width={3}>
+        <Image src='/assets/images/wireframe/image.png' />
+      </Grid.Column>
+      <Grid.Column width={10}>
+        <Image src='/assets/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column width={3}>
+        <Image src='/assets/images/wireframe/image.png' />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
        
 
       <div className="display">
--- client/components/NavigationBar.js
+++ client/components/NavigationBar.js
@@ -1,23 +1,27 @@
 import React, {Component} from 'react';
 import { Link } from 'react-router';
+import { Menu } from 'semantic-ui-react'
 
 class NavigationBar extends Component {
- 
-  render() {
-    return (
-      <nav className="navbar navbar-default">
-        <div className="container-fluid">
-          <div className="navbar-header">
-            <Link to="/" className="navbar-brand">Imperfect Produce</Link>
-            <Link to="/account" className="nav navbar-nav ">My Account</Link>
-            <Link to="/" className="nav-item">Logout</Link>
-          </div>
+  constructor(props) {
+    super(props);
+     this.state = {};
+  }
 
-          <div className="collapse navbar-collapse">
-            
-          </div>
-        </div>
-      </nav>
+  // handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state
+    return (
+      <div >
+        <Menu>
+          <Menu.Item header as={Link} to='/'>Imperfect Produce</Menu.Item>
+          <Menu.Item header as={Link} to='/account'>My Account</Menu.Item>
+          <Menu.Item header as={Link} to='/logout'>Logout</Menu.Item>
+          <Menu.Item name='jobs' active={activeItem === 'jobs'} onClick={this.handleItemClick} />
+          <Menu.Item name='locations' active={activeItem === 'locations'} onClick={this.handleItemClick} />
+        </Menu>
+      </div>
     );
   }
 }
