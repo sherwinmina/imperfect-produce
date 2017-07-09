f0dc90686484f60218e333789a7bd86385c1cd19
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 11:46:29 -0700

    added login page

--- client/components/Main/Login.js
+++ client/components/Main/Login.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Container, Card, Icon, Button, Form, Input, Checkbox } from 'semantic-ui-react'
 
 class Login extends Component {
   render () {
     return (
       <div>
-        Login
+        <Container>
+          <Card>
+            <h3>Log in</h3>
+            <Form>
+              <Form.Field>
+                <label>Email</label>
+                <input placeholder='First Name' />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input placeholder='Password' />
+              </Form.Field>
+              <Button type='submit'>Submit</Button>
+            </Form>
+          </Card>
+        </Container>
+       
       </div>
     )
   }
