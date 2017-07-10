bb99d337f1fe614a0fa2099883b2fb779457b098
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 18:40:23 -0700

    Added delivery schedule omponent

---
+++ client/components/Account/DeliverySchedule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Delivery extends Component {
+  render () {
+    return (
+      <div>
+        Delivery Schedule
+      </div>
+    )
+  }
+}
+
+export default Delivery
