1213d09a76c7c74e70dfdb9117db9baef60f4669
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 18:39:22 -0700

    added billing details

---
+++ client/components/Account/Billing.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Billing extends Component {
+  render () {
+    return (
+      <div>
+        Billing Details
+      </div>
+    )
+  }
+}
+
+export default Billing
