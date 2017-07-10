4ffb143984505fcc77abc1f46a9e9077b39fa298
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 18:49:13 -0700

    Added directory for customizing the box

---
+++ client/components/CustomizeBox/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CustomizeBox extends Component {
+  render () {
+    return (
+      <div>
+        Customize Box
+      </div>
+    )
+  }
+}
+
+export default CustomizeBox;
