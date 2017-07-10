0d71d699ea06314859b9206e5eb6c8d12e267073
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 18:38:27 -0700

    Added account details

---
+++ client/components/Account/AccountDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Deatils extends Component {
+  render () {
+    return (
+      <div>
+        Account Details
+      </div>
+    )
+  }
+}
+
+export default Deatils
