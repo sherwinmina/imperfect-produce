d17428cc45b51f8dfa5b55835627d0daa1a9b3f3
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-09 18:42:57 -0700

    added refer friends component

---
+++ client/components/Account/ReferFriends.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ReferFriends extends Component {
+  render () {
+    return (
+      <div>
+        Refer friends
+      </div>
+    )
+  }
+}
+
+export default ReferFriends
