87be839efa8d34b9eb8a09eea2512d8769e54800
Sherwin Mina <sherwinmina@yahoo.com>
2017-07-10 11:07:18 -0700

    styled jobs component

--- client/components/Main/About.js
+++ client/components/Main/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Button } from 'semantic-ui-react';
+
 const About = () => (
  <div>  
   <h2 style={{textAlign:'center'}}>WHAT WE’RE ALL ABOUT</h2>
@@ -14,7 +15,8 @@ const About = () => (
   <p>
     As a college student, our CEO Ben Simon noticed that a lot of food waste going to waste in the cafeteria at the University of Maryland. To address this problem, he founded the Food Recovery Network, a nonprofit dedicated to preventing waste on college campuses across the country. Since its founding the FRN has expanded to over 180 colleges and universities. Ben Simon met co-founder Ben Chesler during his work at with the FRN, and Ben Chesler helped him scale his mission of fighting food waste nationwide. During a trip to California in 2015, Ben and Ben met their third co-founder, Ron Clark. Ron had spent decades working in the produce industry, most recently working for the California Association of Food Banks. At the CAFB, Ron developed the “Farm to Family” program that now sources 125 million pounds a year of "ugly produce" to the food banks within the state of California. Through this work Ron had built out a supply chain of 70 to 80 California growers and had been sourcing a large amount of produce. Yet he realized that the quantities that he was getting to food banks were still a drop in the bucket compared to the billions of pounds that never left the farm went to waste. Ben, Ben, and Ron realized that together they could make a big impact on the amount of produce that goes to waste every year because of appearances by sourcing it directly from farms and delivering it at a discount to customers homes. They joined forces and founded Imperfect on August 8th, 2015, with the vision of finding a home for the 6 billion pounds of ugly produce that goes to waste each year in California alone.
   </p>
- 
+  <Button content='View Open Position'>button</Button>
+  he
  </div>
 );
 
--- client/components/Main/Jobs.js
+++ client/components/Main/Jobs.js
@@ -1,14 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Button, Container} from 'semantic-ui-react';
 
 class Jobs extends Component {
   render () {
     return (
-      <div style={{paddingTop: '50px'}}>
+      <Container text style={{paddingTop: '50px'}}>
         <h2 style={{textAlign:'center'}}>JOIN THE TEAM!</h2>
         <p>
          Imperfect is a mission-driven startup that is creating America's first major consumer brand for "ugly" produce. One in five fruits and vegetables grown in America never make it off the farm due to tiny cosmetic defects that do not affect quality. Imperfect is on a mission to find a home for these misshapen fruits and veggies in people's fridges by selling them for a 30-50% discount with a lovable, hip brand. We offer the cheapest and most environmentally friendly produce option, starting with our weekly produce delivery service in the Bay Area. In addition to building a scalable solution to fight food waste, our business model also generates extra revenue to California farmers. Imperfect has been featured on the Today Show, The New York Times, National Geographic, PBS NewsHour, NPR, Washington Post, Food and Wine, and the front page of the SF Chronicle. We have put together a team of the leading food waste entrepreneurs in the country and are working with world-class advisors, including Doug Rauch, former President of Trader Joe's.
         </p>
-      </div>
+        <Container textAlign='center' style={{paddingTop: '60px', paddingBottom: '30px'}}> 
+          <Button size='large' primary content='View Open Position'/>
+        </Container>
+        
+      </Container>
     )
   }
 }
